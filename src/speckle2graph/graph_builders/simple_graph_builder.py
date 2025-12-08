e38875a90cb43e7bc216b808a6d38a82ae98d293
Arseney <arseneyworking@gmail.com>
2025-12-08 13:10:28 +0100

    User accessibility to a certain methods was changed

--- src/speckle2graph/graph_builders/simple_graph_builder.py
+++ src/speckle2graph/graph_builders/simple_graph_builder.py
@@ -13,29 +13,29 @@ import numpy as np
 
 class GraphBuilder:
     def __init__(self, traversed_speckle_object):
-        self.traversed_speckle_object: TraverseRevitDAG = traversed_speckle_object
-        self.logical_objects: dict = {}
-        self.geometrical_objects: dict = {}
+        self._traversed_speckle_object: TraverseRevitDAG = traversed_speckle_object
+        self._logical_objects: dict = {}
+        self._geometrical_objects: dict = {}
         self.logical_graph = nx.DiGraph()
         self.geometrical_graph = nx.DiGraph()
 
         p = index.Property()
         p.dimension = 3
-        self.spatial_index = index.Index(properties=p)
+        self._spatial_index = index.Index(properties=p)
     
     def _separate_logical_and_geometrical_objects(self):
-        for speckle_object in self.traversed_speckle_object:
+        for speckle_object in self._traversed_speckle_object:
             if isinstance(speckle_object, LogicalNode):
-                self.logical_objects[speckle_object.id] = speckle_object
+                self._logical_objects[speckle_object.id] = speckle_object
             elif isinstance(speckle_object, GeometryNode):
-                self.geometrical_objects[speckle_object.id] = speckle_object
+                self._geometrical_objects[speckle_object.id] = speckle_object
 
     def build_logical_graph(self, edge_type="CONTAINS"):
-        if self.logical_objects == {}:
+        if self._logical_objects == {}:
             logger.info("Calling a method to separate logical and geometrical elements")
             self._separate_logical_and_geometrical_objects()
 
-        for key, value in self.logical_objects.items():
+        for key, value in self._logical_objects.items():
             for contained_element in value.containedElementsIds:
                 self.logical_graph.add_node(contained_element, id=contained_element)
                 self.logical_graph.add_edge(key, contained_element, name=edge_type)
@@ -47,8 +47,8 @@ class GraphBuilder:
         )
 
     def _build_geometries_index(self):
-        for i, obj in enumerate(self.geometrical_objects.values()):
-            self.spatial_index.insert(i, obj.bounding_box, obj=obj.id)
+        for i, obj in enumerate(self._geometrical_objects.values()):
+            self._spatial_index.insert(i, obj.bounding_box, obj=obj.id)
 
     def _find_intersection_pairs(self):
         from trimesh.collision import CollisionManager
@@ -56,12 +56,12 @@ class GraphBuilder:
 
         self._build_geometries_index()
 
-        for element in tqdm(self.geometrical_objects.values(), desc="Finding intersecting geometries"):
-            for intersection in self.spatial_index.intersection(element.bounding_box, objects=True):
+        for element in tqdm(self._geometrical_objects.values(), desc="Finding intersecting geometries"):
+            for intersection in self._spatial_index.intersection(element.bounding_box, objects=True):
                 if element.id != intersection.object:
                     collisionManager = CollisionManager()
                     collisionManager.add_object(name = element.id, mesh = element.geometry)
-                    collisionManager.add_object(name = intersection.object, mesh = self.geometrical_objects[intersection.object].geometry)
+                    collisionManager.add_object(name = intersection.object, mesh = self._geometrical_objects[intersection.object].geometry)
                     result = collisionManager.in_collision_internal(return_names=True)
                     if result[0]:
                         intersection_pairs.add(tuple(*result[1]))
@@ -70,10 +70,10 @@ class GraphBuilder:
 
     
     def build_geometrical_graph(self, edge_type="CONNECTED_TO"):
-        if len(self.geometrical_objects) == 0:
+        if len(self._geometrical_objects) == 0:
             self._separate_logical_and_geometrical_objects()
 
-        for obj in self.geometrical_objects.values():
+        for obj in self._geometrical_objects.values():
             try:
                 node_properties = json.loads(obj.properties)
             except json.JSONDecodeError as e:
@@ -92,8 +92,8 @@ class GraphBuilder:
         intersection_pairs = self._find_intersection_pairs()
 
         for pair in intersection_pairs:
-            first_centroid = self.geometrical_objects[pair[0]].centroid
-            second_centroid = self.geometrical_objects[pair[1]].centroid
+            first_centroid = self._geometrical_objects[pair[0]].centroid
+            second_centroid = self._geometrical_objects[pair[1]].centroid
             centroid_based_distance = np.linalg.norm(second_centroid - first_centroid)
     
             self.geometrical_graph.add_edge(pair[0], pair[1], name = edge_type, distance = centroid_based_distance)
@@ -103,3 +103,6 @@ class GraphBuilder:
             self.geometrical_graph.number_of_nodes(),
             self.geometrical_graph.number_of_edges()
         )
+
+    def get_geometries(self):
+        return [i for i in self._geometrical_objects.values()]
